3f161a4c2634a898e28c12db8520d1b4e02de46f
Andy.McCullough <andy.mccullough@ultima.com>
2021-11-18 15:47:02 +0000

    terrafmt

--- internal/services/desktopvirtualization/virtual_desktop_host_pool_registration_info_resource_test.go
+++ internal/services/desktopvirtualization/virtual_desktop_host_pool_registration_info_resource_test.go
@@ -102,8 +102,8 @@ resource "azurerm_virtual_desktop_host_pool" "test" {
 }
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "test" {
-  hostpool_id		   = azurerm_virtual_desktop_host_pool.test.id
-  expiration_date	   = timeadd(timestamp(), "48h")
+  hostpool_id     = azurerm_virtual_desktop_host_pool.test.id
+  expiration_date = timeadd(timestamp(), "48h")
 }
 
 
@@ -112,29 +112,29 @@ resource "azurerm_virtual_desktop_host_pool_registration_info" "test" {
 
 func (VirtualDesktopHostPoolRegistrationInfoResource) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
-	provider "azurerm" {
-		features {}
-	  }
-	  
-	  resource "azurerm_resource_group" "test" {
-		name     = "acctestRG-vdesktop-%d"
-		location = "%s"
-	  }
-	  
-	  resource "azurerm_virtual_desktop_host_pool" "test" {
-		name                 = "acctestHP%s"
-		location             = azurerm_resource_group.test.location
-		resource_group_name  = azurerm_resource_group.test.name
-		type                 = "Pooled"
-		validate_environment = true
-		load_balancer_type   = "BreadthFirst"
-	  
-	  }
-	  
-	  resource "azurerm_virtual_desktop_host_pool_registration_info" "test" {
-		hostpool_id		   = azurerm_virtual_desktop_host_pool.test.id
-		expiration_date	   = timeadd(timestamp(), "72h")
-	  }
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctestRG-vdesktop-%d"
+  location = "%s"
+}
+
+resource "azurerm_virtual_desktop_host_pool" "test" {
+  name                 = "acctestHP%s"
+  location             = azurerm_resource_group.test.location
+  resource_group_name  = azurerm_resource_group.test.name
+  type                 = "Pooled"
+  validate_environment = true
+  load_balancer_type   = "BreadthFirst"
+
+}
+
+resource "azurerm_virtual_desktop_host_pool_registration_info" "test" {
+  hostpool_id     = azurerm_virtual_desktop_host_pool.test.id
+  expiration_date = timeadd(timestamp(), "72h")
+}
 
 `, data.RandomInteger, data.Locations.Secondary, data.RandomString)
 }
